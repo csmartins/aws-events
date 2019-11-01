417f2cf4c18ffe2ec2ddc1e6a7e005df87eb3a8f
Carlos Martins <cmartins@stone.com.br>
2019-11-01 10:13:38 -0300

    Forgot to create instances

--- infra/documentdb.tf
+++ infra/documentdb.tf
@@ -4,3 +4,10 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = "${var.mongouser}"
   master_password         = "${var.mongopassword}"
 }
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "docdb-cluster-events-${count.index}"
+  cluster_identifier = "${aws_docdb_cluster.docdb.id}"
+  instance_class     = "db.r4.large"
+}
